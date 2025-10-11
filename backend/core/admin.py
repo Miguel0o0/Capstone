c4e794466cc0c36830f097860f3266b844260fa0
MiguelAngel1341 <mi.medina@duocuc.cl>
2025-10-11 18:25:15 -0300

    Creacion de los modelos bases de residentes y hogares
    se dejan listos los dos modelos bases y esenciales.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Resident, Household
 
-# Register your models here.
+@admin.register(Resident)
+class ResidentAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "email", "telefono", "activo")
+    search_fields = ("nombre", "email")
+    list_filter = ("activo",)
+
+@admin.register(Household)
+class HouseholdAdmin(admin.ModelAdmin):
+    list_display = ("direccion", "numero", "referencia")
+    search_fields = ("direccion",)
